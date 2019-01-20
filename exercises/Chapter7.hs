651fd4f0cbf696a0ee9c88a34365af1e6f074e42
Juan Pablo Royo Sales <juanpablo.royo@schibsted.com>
2019-01-20 13:25:48 +0100

    Starting chapter 7

---
+++ exercises/Chapter7.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE ConstraintKinds      #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeApplications     #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE RankNTypes           #-}
+
+module Chapter7 where
+
+data Any where
+  Any :: a -> Any
+
+elimAny :: (forall a. a -> r) -> Any -> r
+elimAny f (Any a) = f a
+
+data HasShow where
+  HasShow :: Show t => t -> HasShow
+
+--instance Show HasShow where
+--  show (HasShow s) = "HasShow " ++ show s
+
+elimHasShow :: (forall a. Show a => a -> r) -> HasShow -> r
+elimHasShow f (HasShow a) = f a
+
+-- Exercise 7.1.iii
+instance Show HasShow where
+  show = elimHasShow show
+
+  
